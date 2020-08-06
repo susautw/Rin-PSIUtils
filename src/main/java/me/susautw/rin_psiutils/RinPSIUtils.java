ce1379a3b8ceaa1ef46df45b0d1491270877e94b
Su-Rin <susautw@gmail.com>
2020-08-07 03:42:59 +0800

    Setup environments Downgrade forge gradle
    Modify: build.gradle, gradle.properties, mcmod.info, pack.mcmeta
    New: RinPSIUtils.java
      The entry point of the mod

--- src/main/java/me/susautw/rin_psiutils/RinPSIUtils.java
+++ src/main/java/me/susautw/rin_psiutils/RinPSIUtils.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.susautw.rin_psiutils;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,19 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(
+        modid = RinPSIUtils.MODID,
+        name = RinPSIUtils.NAME,
+        version = RinPSIUtils.VERSION,
+        dependencies = "required-after:psi@[${psi_version},);" +
+                "required-after:forge@[${forge_version},);",
+        useMetadata = true
+)
+public class RinPSIUtils
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "${mod_id}";
+    public static final String NAME = "${mod_name}";
+    public static final String VERSION = "${mod_version}";
 
     private static Logger logger;
 
